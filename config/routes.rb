d07eb12642149632c8ae810374b395109f6beb81
Patrick Kim <pkim050@gmail.com>
2019-05-14 19:43:06 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists
+    resources :songs, only: %i[show index new edit]
+  end
+
+  resources :songs
+
+  namespace :admin do
+    resources :preferences
+  end
 end
